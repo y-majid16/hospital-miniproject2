b86a39fa0580c1b5fbcfeec9725937bdb3fbb513
y-majid16 <yaumulmajid@gmail.com>
2023-06-25 04:17:09 +0700

    remove devendency

--- src/main/java/id/co/indivara/miniproject/hospital/controllers/PatientController.java
+++ src/main/java/id/co/indivara/miniproject/hospital/controllers/PatientController.java
@@ -7,7 +7,6 @@ import id.co.indivara.miniproject.hospital.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
